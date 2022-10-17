b361b4ec21871373a4e5871fdc29e86b770e6feb
James Giddings <james.p.giddings@gmail.com>
2022-10-17 13:30:14 +0100

    added annotations to service

--- help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/service/TicketServiceImp.java
+++ help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/service/TicketServiceImp.java
@@ -2,9 +2,15 @@ package com.fdmgroup.helpdeskapi.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.fdmgroup.helpdeskapi.model.Ticket;
 import com.fdmgroup.helpdeskapi.repository.TicketRepository;
 
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class TicketServiceImp implements TicketService {
 
     TicketRepository ticketRepository;
