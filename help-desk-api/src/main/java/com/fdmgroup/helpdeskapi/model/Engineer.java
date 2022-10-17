e14eb6110ef340a35fbcb20361bbc1ce41b44a2e
Laney Deveson <laney.deveson@fdmgroup.com>
2022-10-17 15:49:26 +0100

    Added user models

---
+++ help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/model/Engineer.java
@@ -0,0 +1,14 @@
+package com.fdmgroup.helpdeskapi.model;
+
+public class Engineer extends User {
+	private String specialism;
+
+	public String getSpecialism() {
+		return specialism;
+	}
+
+	public void setSpecialism(String specialism) {
+		this.specialism = specialism;
+	}
+
+}
