5e978fe16c2cb2bdd6e58592feac790e0f5ae1cb
James Giddings <james.p.giddings@gmail.com>
2022-10-18 20:01:19 +0100

    loggers added again

--- help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/service/MessageServiceImp.java
+++ help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/service/MessageServiceImp.java
@@ -2,6 +2,8 @@ package com.fdmgroup.helpdeskapi.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.fdmgroup.helpdeskapi.model.Message;
@@ -13,25 +15,30 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class MessageServiceImp implements MessageService {
 
+    Logger logger = LoggerFactory.getLogger(MessageServiceImp.class);
     MessageRepository messageRepository;
 
     @Override
     public Message saveMessage(Message message) {
+        logger.info("MessageServiceImp - Saving message: {}", message);
         return messageRepository.save(message);
     }
 
     @Override
     public List<Message> findAllMessages() {
+        logger.info("MessageServiceImp - Finding all messages");
         return messageRepository.findAll();
     }
 
     @Override
     public Message findMessageById(long id) {
+        logger.info("MessageServiceImp - Finding message by id: {}", id);
         return messageRepository.findById(id).orElse(null);
     }
 
     @Override
     public void deleteMessageById(long id) {
+        logger.info("MessageServiceImp - Deleting message by id: {}", id);
         messageRepository.deleteById(id);
     }
 
--- help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/service/TicketServiceImp.java
+++ help-desk-api/src/main/java/com/fdmgroup/helpdeskapi/service/TicketServiceImp.java
@@ -2,6 +2,8 @@ package com.fdmgroup.helpdeskapi.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.fdmgroup.helpdeskapi.model.Ticket;
@@ -13,42 +15,48 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TicketServiceImp implements TicketService {
 
+	Logger logger = LoggerFactory.getLogger(TicketServiceImp.class);
 	TicketRepository ticketRepository;
 
 	@Override
 	public Ticket saveTicket(Ticket ticket) {
+		logger.info("TicketServiceImp - Saving ticket: {}", ticket);
 		return ticketRepository.save(ticket);
 	}
 
 	@Override
 	public List<Ticket> findAllTickets() {
+		logger.info("TicketServiceImp - Finding all tickets");
 		return ticketRepository.findAll();
 	}
 
 	@Override
 	public Ticket findTicketById(long id) {
+		logger.info("TicketServiceImp - Finding ticket by id: {}", id);
 		return ticketRepository.findById(id).orElse(null);
 	}
 
 	@Override
 	public void deleteTicketById(long id) {
+		logger.info("TicketServiceImp - Deleting ticket by id: {}", id);
 		ticketRepository.deleteById(id);
 	}
 
 	@Override
 	public List<Ticket> findTicketsByEngineerId(Long id) {
+		logger.info("TicketServiceImp - Finding tickets by engineer id: {}", id);
 		return ticketRepository.findTicketsByEngineerId(id);
 	}
 
 	@Override
 	public List<Ticket> findTicketsByClientId(Long id) {
-		// TODO Auto-generated method stub
+		logger.info("TicketServiceImp - Finding tickets by client id: {}", id);
 		return ticketRepository.findTicketsByClientId(id);
 	}
 
 	@Override
 	public List<Ticket> findAllUnassignedTickets() {
-		// TODO Auto-generated method stub
+		logger.info("TicketServiceImp - Finding all unassigned tickets");
 		return ticketRepository.findTicketsByEngineerIdIsNullAndResolvedFalse();
 	}
 
