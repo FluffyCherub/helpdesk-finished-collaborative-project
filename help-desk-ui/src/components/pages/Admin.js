960e173a3f5428fb42f87b52f839fe83ff530877
James Giddings <james.p.giddings@gmail.com>
2022-10-19 09:33:36 +0100

    admin view shows unassigned tickets

--- help-desk-ui/src/components/pages/Admin.js
+++ help-desk-ui/src/components/pages/Admin.js
@@ -1,36 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Admin = () => {
-  return (
-    <div>
-      <h1 className="display-4">Employee Portal</h1>
-      <p className="lead">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-        doloribus tempora sed iure quibusdam officiis quasi accusamus, a
-        exercitationem impedit corrupti voluptas est tenetur magni? Labore quis
-        commodi accusamus amet?
-      </p>
-      <p className="text-secondary">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
-        officiis eius obcaecati incidunt reiciendis exercitationem sint iusto
-        rem architecto? Recusandae ullam, sint aliquid id sed excepturi
-        molestiae atque explicabo. Amet. Harum quos id ullam optio minus
-        laudantium officiis, error repellat repellendus atque blanditiis fuga
-        sed aliquid sit delectus sapiente facilis. Dicta, quo? Unde itaque rerum
-        aliquam voluptatum, quia quasi aliquid. Nulla consectetur facere
-        voluptates, ad recusandae maiores aliquid numquam autem rem quasi ipsam.
-        Laborum consequuntur, repellat excepturi cum facere odit corrupti! Eum
-        nostrum ut provident laudantium labore vero iusto earum. Distinctio
-        vitae cum veniam blanditiis molestias minus voluptates et molestiae
-        corrupti eos quibusdam reprehenderit itaque laborum maxime, fugit sed
-        porro suscipit, animi soluta. Dolorem, qui accusamus deserunt expedita
-        quia id! Odio commodi repellat optio consequuntur eaque numquam. Quaerat
-        similique nobis in maiores suscipit amet ullam earum omnis reprehenderit
-        aliquid obcaecati, culpa recusandae totam modi iure eos magnam enim
-        tempora adipisci?
-      </p>
-    </div>
-  );
-};
+import axios from "axios";
+import Ticket from "../Ticket";
 
+class Admin extends Component {
+  state = {
+    tickets: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8081/gateway/tickets/unassigned")
+      .then((response) => this.setState({ tickets: response.data }));
+  }
+
+  deleteTicket = (id) => {
+    axios.delete("http://localhost:8081/gateway/tickets/" + id);
+  };
+
+  render() {
+    const { tickets } = this.state;
+    return (
+      <React.Fragment>
+        {tickets.map((ticket) => (
+          <Ticket
+            key={ticket.id}
+            ticket={ticket}
+            handleDeleteTicket={this.deleteTicket.bind(this, ticket.id)}
+          />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
 export default Admin;
